bbb9a4188c1bbd0808bcb15f4ba073213846ef9d
Ubuntu <ubuntu@ip-172-31-37-16.us-west-2.compute.internal>
2020-07-30 17:45:35 +0000

    something working lmao

--- app/trixdemo.py
+++ app/trixdemo.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, request, redirect
+import os
+from PIL import Image
+from datetime import datetime
+
 app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+import random
+import string
+random.seed(datetime.now())
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
 @app.route('/')
 def hello_world():
   return 'Hello from Flask!'
+
+@app.route('/multiply/<num>')
+def multiply(num):
+    return 'Result: ' + str(int(num)*12)
+@app.route("/process", methods=["POST"])
+def process():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save('/home/ubuntu/fawkes/app/' + uploaded_file.filename)
+    return "That worked!"
+
 if __name__ == '__main__':
   app.run()
