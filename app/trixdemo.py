95ea56eed960c2ea3224b2d0de6ed5fa3dd6f82c
Ubuntu <ubuntu@ip-172-31-37-16.us-west-2.compute.internal>
2020-08-01 07:14:43 +0000

    finally?

--- app/trixdemo.py
+++ app/trixdemo.py
@@ -54,4 +54,5 @@ def process(mode):
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="0.0.0.0", port=80, ssl_context='adhoc')
+    context = ('server.crt', 'server.key')
+    app.run(host="0.0.0.0", port=80, ssl_context=context)
